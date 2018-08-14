a6deba6f0c7aceda2752cc5121352a50e0021c50
spetsmash <spetsmash@list.ru>
2018-08-14 12:53:24 +0300

    change site

--- routes/index.js
+++ routes/index.js
@@ -222,7 +222,7 @@ function sendEmailVerification (chat, email, id) {
     models.User
         .update({token: id}, {where: {chatId: chat, email: email}})
         .spread((user, created) => {
-            const link = "http://34.245.133.13:3000"+ "/verify?id=" + id + "&email="+ email;
+            const link = "https://ini-mw.act.foundation"+ "/verify?id=" + id + "&email="+ email;
             const message = {
                 to: email,
                 from: 'usinvestors@notallowed.com',
@@ -279,7 +279,7 @@ router.get('/verify',function(req,res){
     // console.log(req.protocol+":/"+req.get('host'));
     // console.log(req);
     // console.log(req.query.email);
-    if((req.protocol+"://"+req.get('host')) == ("http://34.245.133.13:3000"))
+    if((req.protocol+"://"+req.get('host')) == ("https://ini-mw.act.foundation"))
     {
         console.log("Domain is matched. Information is from Authentic email");
         models.User.findOne({ where: {email: req.query.email} }).then(project => {
